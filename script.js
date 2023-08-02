47e0276826efac94c73325b4f45c2c0012b220c4
vinayancha97 <135926111+vinayancha97@users.noreply.github.com>
2023-08-02 22:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const line = document.getElementById('line');
+  let rotation = 0;
+
+  function rotateLine() {
+    rotation += 1;
+    line.style.transform = `rotate(${rotation}deg)`;
+    requestAnimationFrame(rotateLine);
+  }
+
+  rotateLine();
+});
